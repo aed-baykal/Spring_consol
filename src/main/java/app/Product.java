0cac4909c4710646c4ecf02d88dbf4f1b7944885
Andrey <aedbaykal@gmail.com>
2021-12-09 12:35:10 +0300

    д/з ко второму уроку

---
+++ src/main/java/app/Product.java
@@ -0,0 +1,47 @@
+package app;
+
+public class Product {
+    private Integer id;
+    private String title;
+    private Float cost;
+
+    public Product(Integer id, String title, Float cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getCost() {
+        return cost;
+    }
+
+    public void setCost(Float cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+
+}
